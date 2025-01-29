163f7155a8c9aba989180879da515acc07b0d927
Jawad <jawadal829@gmail.com>
2025-01-29 15:11:53 +0500

    updation in ui

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# skill-swap
+# Skillswap
+A simple web service to swap skills and share it with other users to get learn new things.
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,7 +4,8 @@ import "../style/login.scss";
 import { useState } from "react";
 
 const Login = ({ onClose }) => {
-    const [showPassword, setShowPassword] = useState(true)
+  const [showPassword, setShowPassword] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <div className="login-container flex" onClick={onClose}>
       <div
@@ -14,23 +15,31 @@ const Login = ({ onClose }) => {
         <div className="logo flex">
           <GoLink />
         </div>
-        <h1>Share Up</h1>
+        <h1>Hi There!</h1>
         <div className="form flex col">
           <div className="input-wrapper flex">
             <input type="text" placeholder="Email Address" />
           </div>
           <div className="input-wrapper flex">
-            <input type={showPassword ? "text" : "password"} placeholder="Password" />
-            <div className="text flex" onClick={() => setShowPassword(!showPassword)}>
-                {
-                    showPassword ? "HIDE" : "SHOW"
-                }
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+            />
+            <div
+              className="text flex"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? "HIDE" : "SHOW"}
             </div>
           </div>
-          <div className="forgot-text flex">
-            Forgot Password?
-          </div>
-          <div className="btn-login flex">LOgin</div>
+          <div className="forgot-text flex">Forgot Password?</div>
+          <button
+            className="btn-login flex"
+            disabled={isLoading}
+            style={{ cursor: isLoading ? "not-allowed" : "pointer" }}
+          >
+            {isLoading ? <div className="loader flex"></div> : "Login"}
+          </button>
         </div>
       </div>
     </div>
